bede8810c7d44f8a587eba1315971d2392f540d0
Linnea <linnea@klangby.com>
2020-09-23 11:08:16 +0200

    fixat xml serialisering

---
+++ savingGrejs/Arena.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace savingGrejs
+{
+    public class Arena
+    {
+        public string name = "";
+        public int peopleAmount = 0;
+        public int peopleHappy = 0;
+    }
+}
--- savingGrejs/Program.cs
+++ savingGrejs/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace savingGrejs
 {
@@ -6,7 +9,25 @@ namespace savingGrejs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Arena a1 = new Arena();
+            Console.WriteLine("Vad ska din arena heta?");
+            a1.name = Console.ReadLine();
+            Console.WriteLine("Hur glad ska publiken vara? siffra");
+            a1.peopleAmount = Check(Console.ReadLine());
+            Console.WriteLine("Hur glada ska de vara? siffra");
+            a1.peopleHappy = Check(Console.ReadLine());
+
+            XmlSerializer arenaSerializer = new XmlSerializer(typeof(Arena));
+            FileStream file = File.Open("arena.xml", FileMode.OpenOrCreate);
+            arenaSerializer.Serialize(file, a1);
+            file.Close();
+
+        }
+        static int Check (string tal)
+        {
+            int resultat;
+            bool lyckad = int.TryParse(tal, out resultat);
+            return resultat;
         }
     }
 }
